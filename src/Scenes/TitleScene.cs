052744999d2713fd449d48769df017f687a7d5ed
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2024-02-23 18:45:29 +0900

    メニューをリニューアル

--- src/Scenes/TitleScene.cs
+++ src/Scenes/TitleScene.cs
@@ -12,37 +12,97 @@ public class TitleScene(
     Resources resources,
     InputService input) : Scene
 {
+    private Text menu;
+    private int index;
+    private (string, Action)[] mainMenuItems;
+    private (string, Action)[] gamemodeMenuItems;
+    private (string, Action)[] currentItems;
+
     public override void OnStart()
     {
         window.Title = "Sukiteto";
         window.Size = (640, 480);
-        
-        var logo = new Sprite(resources.Logo);
-        logo.Location = (window.Width / 2 - logo.Width / 2, 72);
+        App.BackgroundColor = Color.FromArgb(20, 20, 20);
 
-        var buttonStart = new Button("マラソン", Color.FromArgb(44, 110, 61), Color.White, (window.Width - 32, 64))
-            .Location(16, 180);
-        buttonStart.Click += app.LoadScene<GameScene>;
-        
-        var buttonConfig = new Button("設定", Color.FromArgb(90, 90, 90), Color.White, (window.Width - 32, 64))
-            .Location(16, buttonStart.Location.Y + 64 + 16);
-        buttonConfig.Click += app.LoadScene<ConfigScene>;
-        
-        var buttonExit = new Button("おわる", Color.FromArgb(156, 48, 50), Color.White, (window.Width - 32, 64))
-            .Location(16, buttonConfig.Location.Y + 64 + 16);
-        buttonExit.Click += () => app.Exit();
-
-        var copyright = new Text($"(C)2023-2024 Ebise Lutica and GitHub contributors\nversion {Global.Version}", Font.GetDefault(14), Color.LightGray);
-        copyright.Location = (window.Width - copyright.Width - 2, window.Height - copyright.Height - 2);
-
-        Root.AddRange(logo, buttonStart, buttonConfig, buttonExit, copyright);
+        InitializeUI();
+        InitializeMenuItems();
     }
 
     public override void OnUpdate()
     {
+        if (input[InputType.MenuUp].IsButtonUp)
+        {
+            index--;
+            if (index < 0) index = currentItems.Length - 1;
+            UpdateMenu();
+        }
+        if (input[InputType.MenuDown].IsButtonUp)
+        {
+            index++;
+            if (index >= currentItems.Length) index = 0;
+            UpdateMenu();
+        }
         if (input[InputType.Ok].IsButtonUp)
         {
-            app.LoadScene<GameScene>();
+            currentItems[index].Item2();
         }
+    }
+    
+    private void InitializeUI()
+    {
+        var logo = new Sprite(resources.Logo)
+            .Scale(2, 2)
+            .Location(48, 48);
+
+        menu = new Text("", Font.GetDefault(18), Color.White)
+            .Location(48, 192);
+
+        var copyright = new Text($"(C)2023-2024 Ebise Lutica and GitHub contributors\nversion {Global.Version}", Font.GetDefault(14), Color.LightGray);
+        copyright.Location = (window.Width - copyright.Width - 2, window.Height - copyright.Height - 2);
+
+        Root.AddRange(logo, menu, copyright);
+    }
+    
+    private void InitializeMenuItems()
+    {
+        mainMenuItems =
+        [
+            ("ゲームをはじめる", OnEnterGameModeMenu),
+            ("設定", () => app.LoadScene<ConfigScene>()),
+            ("ウィンドウを閉じる", () => app.Exit()),
+        ];
+
+        gamemodeMenuItems =
+        [
+            ("150マラソン", () => app.LoadScene<GameScene>()),
+            ("エンドレス", () => app.LoadScene<GameScene>()),
+            ("40ライン（未実装）", () => { }),
+            ("ミッション（未実装）", () => { }),
+            ("DEATH OF G（未実装）", () => { }),
+            ("←戻る", OnLeaveGameModeMenu)
+        ];
+        currentItems = mainMenuItems;
+
+        UpdateMenu();
+    }
+
+    private void UpdateMenu()
+    {
+        var itemContents = currentItems.Select((item, i) => $"{(index == i ? "→" : "　")} {item.Item1}");
+        menu.Content = string.Join('\n', itemContents);
+    }
+    
+    private void OnEnterGameModeMenu()
+    {
+        currentItems = gamemodeMenuItems;
+        index = 0;
+        UpdateMenu();
+    }
+    
+    private void OnLeaveGameModeMenu()
+    {
+        currentItems = mainMenuItems;
+        index = 0;
+        UpdateMenu();
     }
 }
